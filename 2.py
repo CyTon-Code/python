c244ab701dcf7edc1d2c51e2c85aec578aec6cfc
CyTon Code <cytoncode@gmail.com>
2021-02-18 08:32:08 +0200

    Update

---
+++ 2.py
@@ -0,0 +1,10 @@
+from math import log, cos
+
+x = float(input("x = "))
+c = log(float(input("c = ")))
+a = float(input("a = ")) * c + x**2
+b = x**2 + c
+
+y = (((a*x)** 3)+b)/cos(x)** 3
+
+print(y)
