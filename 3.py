cf578e3a10e0062e01a0e026a9753627e3566425
CyTon Program Languages <cytoncode@gmail.com>
2021-02-18 11:12:21 +0200

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,21 +1,21 @@
 from math import exp, cos, tan
 
 a = 3.7
-p = 0.05
+p = 0.05  # Блок вичислень
 
 i = float(input("i = "))
-n = float(input("n = "))
+n = float(input("n = "))  # блок вводу.выводу
 
-while (i < n):
+while (i < n):  # блок циклу
     x = a * exp(3)*i
-    y = 2 * x * cos(i)
+    y = 2 * x * cos(i)  # блок вичислень
 
-    if (x <= 1):
-        z = (1 + 2 * y*y) * cos(i)
+    if (x <= 1):  # блок розвилки - Так
+        z = (1 + 2 * y*y) * cos(i)  # блок вичелсень
 
-    else:
-        z = pow(7.2 * x, 2) * tan(i)
+    else:  # блок розвилки - Ні
+        z = pow(7.2 * x, 2) * tan(i)  # блок вичеслень
 
-    i += p
+    i += p  # блок вичелсень
 
-    print(z)
+    print(z)  # блок вводу\виводу
