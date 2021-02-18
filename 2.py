7c25a824eb9eab7b9f0bab1bf1935a8ae5f221bc
CyTon Code <cytoncode@gmail.com>
2021-02-18 08:50:20 +0200

    Update

--- 2.py
+++ 2.py
@@ -1,23 +1,20 @@
 from math import exp, cos, tan
 
-# x = 0
-# y = 0
-# z = 0
-a = 3.7
-
-i = float(input("i = "))
-n = float(input("n = "))
+x, y, z=0,0,0
+i = 0.30
+n = 0.60
 p = 0.05
+a = 0
 
-while (i < n):
-    x = a * exp(3)*i
-    y = 2 * x * cos(i)
+while i < n:
+    x = a * exp(3*i)
+    y = 2*x * cos(i)
 
-    if (x <= 1):
-        z = (1 + 2 * y*y) * cos(i)
+    if x <= 1:
+        z = (1 + 2 * y**2) * cos(i)
 
     else:
-        z = pow(7.2 * x, 2) * tan(i)
+        z = (7.2 * x)** 2 * tag(i)
 
     i += p
 
