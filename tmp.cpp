98349304bacefdc9c6d3eda8745126ba89d75fd5
CyTon Program Languages <cytoncode@gmail.com>
2021-02-19 11:07:46 +0200

    Update tmp.cpp

--- tmp.cpp
+++ tmp.cpp
@@ -2,25 +2,22 @@
 #include <iostream>
 #include <string>
 
-using std::cout;
-using std::cin;
+
 using std::string;
 
 string input(string name="What is your name? -")
 {
-    cout << name;
-    
-    getline (cin, name);
-    
+    std::cout << name;
+    getline (std::cin, name);
     return name;
-}
+};
 
 
 void hi(void)
 {
   string name = input();
-  cout << "Hello, " << name << "!\n";
-}
+  std::cout << "Hello, " << name << "!\n";
+};
 
 
 int main(void)
@@ -28,4 +25,4 @@ int main(void)
     hi();
     
     return 0;
-}
+};
